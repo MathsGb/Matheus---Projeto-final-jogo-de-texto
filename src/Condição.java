90c35ef37e4a61719f1092f7f6f6e3f881e25447
MathsGb <matheus.g.v.nova2002@gmail.com>
2022-07-24 14:14:48 -0300

    Segundo commit

--- src/Condição.java
+++ src/Condição.java
@@ -1,13 +1,14 @@
 public class Condição{
-    public static String CalCond(int Atual,int Dano){
+    public static String CalCond(int Atual,int Dano, int Tipo){
+        Atual -= Dano;
         if( Atual <= 0 ){
             return "Game over";
         }
-        else{
-            Atual -= Dano;
-        }
-        return "Integridade atual "+ Integer.toString(Atual)+"/100";
+        else if(Tipo == 1){
+        return "Saúde atual "+ Integer.toString(Atual)+"/100";
     }
-    
-    
+        else{
+            return "Comprometimento atual "+ Integer.toString(Atual)+"/100";
+        }
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,24 +1,39 @@
+import java.util.Scanner;
+
 public class Principal{
     public static void main(String[] args){
+        Scanner pausa = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
+
+        // Entrada e explicação
+
         System.out.println("Nesse jogo voce deverá escolher agentes antes de iniciar, tenha em mente que cada um deles \ntem sua própria história com seus pontos fortes e fracos.\n");
         Agente Alister = new Agente("Alister",
                                     1.90,
                                     40,
                                     "Investigação e informação");
-        System.out.println("Agente 1: \n Nome: " + Alister.nome);
-        System.out.println(" "+ Alister.altura + " m de altura");
-        System.out.println(" "+Alister.idade +" anos de idade");
-        System.out.println(" É especialista em " + Alister.especialidade);
+        System.out.println("Agente 1: \n Nome: " +Alister.nome);
+        System.out.println(" "+Alister.altura+" m de altura");
+        System.out.println(" "+Alister.idade+" anos de idade");
+        System.out.println(" É especialista em " +Alister.especialidade);
 
         Agente Barney = new Agente("Barney",
                                     1.79,
                                     38,
                                     "Combate corpo a corpo e armado");
         System.out.println("\n\nAgente 2: \n Nome: " + Barney.nome);
-        System.out.println(" "+ Barney.altura + " m de altura");
+        System.out.println(" "+ Barney.altura +" m de altura");
         System.out.println(" "+ Barney.idade +" anos de idade");
-        System.out.println(" É especialista em " + Barney.especialidade);
-        System.out.println(Condição.CalCond(100,0));
+        System.out.println(" É especialista em " +Barney.especialidade);
+        System.out.println(Condição.CalCond(100,25,1) + " e " +Condição.CalCond(100,100,2));
 
+        //Início da história 
+
+
+        System.out.println("Qual deles voce vai escolher ?");
+
+    
+    entrada.close();
+    pausa.close();
     }
 }
